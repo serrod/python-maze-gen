817e5dab3928f8f67c29b606c91e5a92abd64a7c
Sergio <se.rod928@gmail.com>
2021-10-17 00:28:05 -0400

    first finish

--- maze.py
+++ maze.py
@@ -13,9 +13,9 @@ pygame.display.set_caption("python maze generation")
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 GRAY = (128,128,128)
-RESOLUTION = 10
+RESOLUTION = 50
 
-FPS = 60
+FPS = 120
 
 class node:
     def __init__(self,x,y):
@@ -45,29 +45,75 @@ def draw(grid):
                 pygame.draw.rect(WINDOW,GRAY,[j*(WIDTH/RESOLUTION),i*(HEIGHT/RESOLUTION),(WIDTH/RESOLUTION)-2,(HEIGHT/RESOLUTION)-2])
             else:
                 pygame.draw.rect(WINDOW,BLACK,[j*(WIDTH/RESOLUTION),i*(HEIGHT/RESOLUTION),(WIDTH/RESOLUTION)-2,(HEIGHT/RESOLUTION)-2])
+            
+            if grid[convert_coords(j,i)].south == False:
+                pygame.draw.rect(WINDOW,BLACK,[j*(WIDTH/RESOLUTION),i*(HEIGHT/RESOLUTION),(WIDTH/RESOLUTION)-2,(HEIGHT/RESOLUTION)])
+            if grid[convert_coords(j,i)].east == False:
+                pygame.draw.rect(WINDOW,BLACK,[j*(WIDTH/RESOLUTION),i*(HEIGHT/RESOLUTION),(WIDTH/RESOLUTION),(HEIGHT/RESOLUTION)-2])
+
+def get_unv(index,grid):
+    unv = []
+    if index-RESOLUTION > 0 and grid[index-RESOLUTION].visited == False:
+        unv.append(index-RESOLUTION)
+
+    if (index+1)%RESOLUTION != 0 and grid[index+1].visited == False:
+        unv.append(index+1)
+
+    if index+RESOLUTION < RESOLUTION*RESOLUTION and grid[index+RESOLUTION].visited == False:
+        unv.append(index+RESOLUTION)
+    
+    if index-1 > 0 and (index-1)%RESOLUTION != RESOLUTION-1 and grid[index-1].visited == False:
+        unv.append(index-1)
+    
+    return unv
 
 def main():
     run = True
     clock = pygame.time.Clock()
     
+    unv = []
     grid = []
     stack = []
     num_vis = 0
+    index = 0
 
     for i in range(RESOLUTION):
             for j in range(RESOLUTION):
                 grid.append(node(j,i))
 
-    grid[5].visited = True
-    grid[26].visited = True
-
-    print(grid[14].visited)
+    stack.append(0)
+    grid[0].visited = True
+    num_vis += 1
 
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        
+        if len(stack) == 0:
+            #end somehow
+            print("end")
+
+        unv = get_unv(stack[-1],grid)
+        if len(unv) != 0:
+            index = random.choice(unv)
+
+            if(index == stack[-1] + 1):
+                grid[stack[-1]].east = False
+            elif(index == stack[-1] + RESOLUTION):
+                grid[stack[-1]].south = False
+            elif(index == stack[-1] - RESOLUTION):
+                grid[stack[-1] - RESOLUTION].south = False
+            elif(index == stack[-1] - 1):
+                grid[stack[-1] - 1].east = False
+            #print(stack[-1] , grid[stack[-1]].east)
+
+            stack.append(index)
+            grid[index].visited = True
+            num_vis += 1
+        else:
+            stack.pop()
 
         draw(grid)
         pygame.display.update()
